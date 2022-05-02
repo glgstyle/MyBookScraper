3994ebc37caf904efe19555ac17903158547c05a
glgstyle glg <gleguenno@gmail.com>
2022-05-02 21:51:03 +0200

    Searching images at the same time that searching books in category

--- scrap_all_photos.py
+++ scrap_all_photos.py
@@ -11,40 +11,33 @@ import csv
 
 # for all books get images, download them and put them in a folder named pictures
 def get_images(image_search_url):
-    response = requests.get(image_search_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    soup_picture = soup.find_all('img', class_='thumbnail')
     pictures = []
     pagination = search_pagination(image_search_url)
-    # if there is more than one page fin all images on every page
-
-    if len(pagination) > 1:
-        for page in pagination:
-            print(page)
-            for image in soup_picture:
-                find_image_url = 'http://books.toscrape.com/' + image.get('src')
-                pictures.append(find_image_url.replace('../../', ''))
-    # else find image on one page
-    else:
-        for image in soup_picture:
+    # loop on every page to find images
+    for page in pagination:
+        print(page)
+        resp = requests.get(page)
+        next_soup = BeautifulSoup(resp.content, 'html.parser')
+        next_soup_picture = next_soup.find_all('img', class_='thumbnail')
+        images = next_soup_picture
+        for image in images:
             find_image_url = 'http://books.toscrape.com/' + image.get('src')
             pictures.append(find_image_url.replace('../../', ''))
 
-    # Essayer de créer le dossier images, si ce n'est pas possible(erreur) ne rien faire(continuer)
-    path = 'images/'
-    try:
-        os.makedirs(path)
-    except OSError:
-        if not os.path.isdir(path):
-            raise
+            # Try to create pictures repertory, if it's not possible(error), dont do anything(continue)
+            path = 'images/'
+            try:
+                os.makedirs(path)
+            except OSError:
+                if not os.path.isdir(path):
+                    raise
 
-    # Pour chaque image dans les images, ouvrir le répertoire images copier les images du site
-    # et les coller dans images/ en reformatant le nom (image1, image2...)
+    # For each picture in pictures, open repertory pictures, copy / paste them inside and refactoring
+    # their name(picture1, picture2...)
     for link in range(len(pictures)):
         img_url = pictures[link]
         print(img_url)
         with open(f'images/image{link+1}.jpg', 'wb+') as f:
             f.write(urllib.request.urlopen(img_url).read())
 
-
-get_images("https://books.toscrape.com/catalogue/category/books/classics_6/index.html")
+get_images("https://books.toscrape.com/catalogue/category/books/fiction_10/")
--- tools.py
+++ tools.py
@@ -55,7 +55,7 @@ def get_article_infos(url):
 
 # Find all books infos in a category
 def find_books_infos_in_category(pagination):
-
+    get_images(pagination)
     all_list_articles_next_page = []
     # loop on every page to find books infos
     for page in pagination:
@@ -104,7 +104,7 @@ def get_categories():
         cat_url = 'https://books.toscrape.com/' + cat.find('a').get('href')
         categories_url.append(cat_url)
     # For each category in ul extract the text of soup, remove blanks around string, save everything to category_name
-    # and remove string'books
+    # and remove string books
         all_a = soup_ul.find_all('a')
     for a in all_a:
         substring = a.text
@@ -149,139 +149,33 @@ def get_category_articles_infos(categoryUrl):
 
 
 def get_images(image_search_url):
-    # url = 'https://books.toscrape.com/'
-    # image_search_url = 'https://books.toscrape.com/catalogue/category/books/business_35/index.html'
-    response = requests.get(image_search_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    soup_picture = soup.find_all('img', class_='thumbnail')
     pictures = []
-
-    # Pour chaque image dans la page, insérer les liens dans le tableau pictures
-    for image in soup_picture:
-        find_image_url = 'http://books.toscrape.com/' + image.get('src')
-        pictures.append(find_image_url.replace('../../', ''))
-
-    return pictures
-
-# :::::::::::::::::
-def get_all_images_in_a_category(category):
-    global category_path
-    pagination = search_pagination(category)
-    all_images_next_page = []
-
-    name_of_category = get_categories()[1]
-    path = 'categoryImages/'
-
-    try:
-        os.makedirs(path)
-    except OSError:
-        if not os.path.isdir(path):
-            raise
-
-    # loop on every page to find books images
+    pagination = search_pagination(image_search_url)
+    # loop on every page to find images
     for page in pagination:
         print(page)
-        find_images = get_images(page)
-        # print(find_images)
-        all_images_next_page.append(find_images)
+        resp = requests.get(page)
+        next_soup = BeautifulSoup(resp.content, 'html.parser')
+        next_soup_picture = next_soup.find_all('img', class_='thumbnail')
+        images = next_soup_picture
+        for image in images:
+            find_image_url = 'http://books.toscrape.com/' + image.get('src')
+            pictures.append(find_image_url.replace('../../', ''))
 
-        for name in name_of_category:
-            category_path = path + name
+            # Try to create pictures repertory, if it's not possible(error), dont do anything(continue)
+            path = 'images/'
             try:
-                os.makedirs(category_path)
+                os.makedirs(path)
             except OSError:
-                if not os.path.isdir(category_path):
+                if not os.path.isdir(path):
                     raise
-        # For each category add the name of category to the path like: categoryCsv/images/Travel
-        # for name in name_of_category:
-        for link_image in range(len(all_images_next_page)):
-            img_url = all_images_next_page[link_image]
-            print(img_url)
-            # with open(category_path + "/" + f'image{link_image + 1}.jpg', 'wb+') as f:
-            for img in img_url:
-                with open('categoryImages' + "/" + f'image{link_image+1}.jpg', 'wb+') as f:
-                    print(img)
-                    f.write(urllib.request.urlopen(img).read())
 
+    # For each picture in pictures, open repertory pictures, copy / paste them inside and refactoring
+    # their name(picture1, picture2...)
+    for link in range(len(pictures)):
+        img_url = pictures[link]
+        print(img_url)
+        with open(f'images/image{link+1}.jpg', 'wb+') as f:
+            f.write(urllib.request.urlopen(img_url).read())
 
-
-
-
-
-    # exit()
-        # with open(category_path + "/" + f'image{link + 1}.jpg', 'wb+') as f:
-        #     for img in range(len(img_url)):
-        #         f.write(urllib.request.urlopen(img).read())
-
-
-        # for data in range(len(data_all_articles)):
-        #     w.writerow(data_all_articles[data])
-
-    return all_images_next_page
-
-
-def get_all_images_from_all_categories():
-    # categories_list = get_categories()[0]
-    # images_category = []
-    # # Find all books images for each category in category list
-    # for category in range(1, len(categories_list)):
-    #     search_images = get_all_images_in_a_category(categories_list[category])
-    #     images_category.append(search_images)
-    # return images_category
-
-    # Try to open categoryCsv/images, if there is no directory images create it
-    name_of_category = get_categories()[1]
-    link_categories = get_categories()[0]
-    # path = 'categoryImages/'
-
-    # path = '\categoryImages'
-    #
-    # try:
-    #     os.makedirs(path)
-    # except OSError:
-    #     if not os.path.isdir(path):
-    #         raise
-
-    # # For each category add the name of category to the path like: categoryCsv/images/Travel
-    # for name in name_of_category:
-    #     # category_path = path + name
-    #     category_path = os.path.join(path, name)
-    #
-    #     try:
-    #         os.makedirs(category_path)
-    #     except OSError:
-    #         if not os.path.isdir(category_path):
-    #             raise
-
-    for link in range(1, len(link_categories)):
-        # img_url = link_categories[link]
-        get_all_images_in_a_category(link_categories[link])
-
-            # filePath = os.path.join(category_path, f'image{link + 1}')
-            # # path_replace = filePath.replace("\", "/")
-            # print(filePath)
-            # with open(filePath, 'wb+') as f:
-            #     f.write(urllib.request.urlopen(img_url).read())
-            # # print(path_replace)
-            # # with open(category_path + f'image{link+1}.jpg', 'wb+') as f:
-
-    # enregistrer les photos dans le dossier de la categorie
-
-    # exit()
-    #
-    # # Pour chaque image dans les images, ouvrir le répertoire images copier les images du site
-    # # et les coller dans images/ en reformatant le nom (image1, image2...)
-    # for link in range(len(pictures)):
-    #     img_url = pictures[link]
-    #     print(img_url)
-    #     with open(f'categoryCsv/+{name_of_category}+images/image{link+1}.jpg', 'wb+') as f:
-    #         f.write(urllib.request.urlopen(img_url).read())
-
-
-# print(get_images('https://books.toscrape.com/catalogue/category/books/business_35/index.html'))
-# print(get_images('https://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html'))
-
-# get_all_images_in_a_category('https://books.toscrape.com/catalogue/category/books/business_35/index.html')
-# get_all_images_in_a_category('https://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html')
-
-# get_all_images_from_all_categories()
+# find_books_infos_in_category("https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html")
